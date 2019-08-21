a11422ff9f3ce822e823197f4d8f55793ca7efe7
Bogdan Cismariu <bogdan.cismariu@gmail.com>
2019-08-21 10:53:23 +0300

    fixed tests

--- tests/XmlToJsonConverterTest.php
+++ tests/XmlToJsonConverterTest.php
@@ -22,23 +22,23 @@ class XmlToJsonConverterTest extends TestCase
         return [
             [
                 '<node attr1="value1" attr2="value2"><child>child value</child></node>',
-                '{"node":{"attributes":{"attr1":"value1","attr2":"value2"},"child":{"value":"child value"}}}'
+                '{"node":{"_attributes":{"attr1":"value1","attr2":"value2"},"child":{"_value":"child value"}}}'
             ],
             [
                 '<node attr1="value1" attr2="value2"><child>child1 value</child><child>child2 value</child></node>',
-                '{"node":{"attributes":{"attr1":"value1","attr2":"value2"},"child":[{"value":"child1 value"},{"value":"child2 value"}]}}'
+                '{"node":{"_attributes":{"attr1":"value1","attr2":"value2"},"child":[{"_value":"child1 value"},{"_value":"child2 value"}]}}'
             ],
             [
                 '<node attr1="value1" attr2="value2"><child1>child1 value</child1><child2>child2 value</child2></node>',
-                '{"node":{"attributes":{"attr1":"value1","attr2":"value2"},"child1":{"value":"child1 value"},"child2":{"value":"child2 value"}}}'
+                '{"node":{"_attributes":{"attr1":"value1","attr2":"value2"},"child1":{"_value":"child1 value"},"child2":{"_value":"child2 value"}}}'
             ],
             [
                 '<node attr1="value1" attr2="value2"><child attr1="value1">child value</child></node>',
-                '{"node":{"attributes":{"attr1":"value1","attr2":"value2"},"child":{"attributes":{"attr1":"value1"},"value":"child value"}}}'
+                '{"node":{"_attributes":{"attr1":"value1","attr2":"value2"},"child":{"_attributes":{"attr1":"value1"},"_value":"child value"}}}'
             ],
             [
                 '<node attr1="value1" attr2="value2"><child attr1="value1">child value</child><child attr1="value2">child value2</child></node>',
-                '{"node":{"attributes":{"attr1":"value1","attr2":"value2"},"child":[{"attributes":{"attr1":"value1"},"value":"child value"},{"attributes":{"attr1":"value2"},"value":"child value2"}]}}'
+                '{"node":{"_attributes":{"attr1":"value1","attr2":"value2"},"child":[{"_attributes":{"attr1":"value1"},"_value":"child value"},{"_attributes":{"attr1":"value2"},"_value":"child value2"}]}}'
             ]
         ];
     }
